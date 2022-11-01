62fc6ab0c97e064e9e5308bf1a729c318915d492
rookiebag <creeppeepcode@gmail.com>
2022-11-01 17:09:55 +0000

    Username added in registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,6 +18,15 @@
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
 
+            <!-- Username -->
+            <div class="mt-4">
+                <x-input-label for="username" :value="__('Username')" />
+
+                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
+
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            </div>
+
             <!-- Email Address -->
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')" />
